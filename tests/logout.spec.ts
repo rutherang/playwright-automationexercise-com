d54a3531f7be6281b7e5b463f14503937cbf99a5
Ruther Ford Myler Ang <ruther.ang@outlook.com>
2026-07-29 23:12:39 +0800

    fix refactor of login
    made the function Login a small case

--- tests/logout.spec.ts
+++ tests/logout.spec.ts
@@ -4,7 +4,7 @@ import { LoginPage } from '../src/pages/login';
 
 test('TC#4 Logout User', async ({ page }) => {
   await page.goto('/login');
-  await new LoginPage(page).Login(VALID_USER);
+  await new LoginPage(page).login(VALID_USER);
   await expect(page.getByText('Logged in as John Doe')).toBeVisible();
   await page.getByRole('link', { name: 'Logout' }).click();
   await expect(page.getByRole('heading', { name: 'Login to your account' })).toBeVisible();
