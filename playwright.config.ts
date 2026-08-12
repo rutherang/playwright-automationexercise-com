ac38829b7d610401529228468e873d37d9401fec
Ruther Ford Myler Ang <ruther.ang@outlook.com>
2026-08-12 10:59:57 +0800

    Update playwright config
    Enabled video
    update trace to 'retain on failure' instead of 'on-first-try'
    add timeout and actionTimeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,44 +1,36 @@
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+  globalSetup: path.join(__dirname, 'src', 'setup', 'global-setup.ts'),
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://www.automationexercise.com',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  reporter: process.env.CI
+    ? [
+        ['html'],
+        ['junit', { outputFile: 'test-results/test-results.xml' }],
+        ['list']
+      ] : [['html'], ['list']],
+  expect: {
+    timeout: 15_000, // 15s for assertion polling (default is 5s)
   },
-
-  /* Configure projects for major browsers */
+  use: {
+    baseURL: 'https://www.automationexercise.com',
+    trace: 'retain-on-failure',
+    video: 'on-first-retry',
+    actionTimeout: 30_000, // 30s for actions like click, fill, hover (default is 0 = falls back to test timeout)
+  },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
-        headless: false
-      },
+      use: { ...devices['Desktop Chrome'], headless: true },
     },
 
     // {
